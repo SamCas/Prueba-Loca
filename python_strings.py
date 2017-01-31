d6f009a9111f6218b11f3135485a4ff5ea5d84a5
Samuel Castillo <samuel.cas.96@gmail.com>
2017-01-31 11:42:25 -0600

    Seeing the slice as a loop

--- python_strings.py
+++ python_strings.py
@@ -13,6 +13,9 @@ print(x)
 # Indexing.
 x = "Hello World"
 print("This is the first letter: ", x[0])
+print("This is the last letter of the string: ", x[-1])
 
 # Slicing.
 print("This is the string after 'Hello'", x[5:])
+print("This is before World", x[:5])
+print("This is the string without the last letter: ", x[:-1])
