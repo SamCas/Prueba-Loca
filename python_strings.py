50dc671e1429b9dd058c0779770e47a57b6b0e21
Samuel Castillo <samuel.cas.96@gmail.com>
2017-01-31 11:30:03 -0600

    Strings and Indexing

--- python_strings.py
+++ python_strings.py
@@ -1,3 +1,15 @@
 # Printing on Python 3 whit parentesis, in Python 2 just with "" or ''.#
 print("Hello! It's me Mario")
 print("LEWL")
+
+# New lines.
+print("Here is a new line \n and this is another, but they are in the same print.")
+print("And this is \t a tab.")
+
+# String Basics.
+x = len("Hello World")
+print(x)
+
+# Indexing.
+x = "Hello World"
+print("This is the first letter: ", x[0])
