c44efa8cd8432e3d5e4fa356fdf2562ca6f054eb
Samuel Castillo <samuel.cas.96@gmail.com>
2017-02-03 15:57:01 -0600

    Adding print lesson.

---
+++ print.py
@@ -0,0 +1,20 @@
+# Print Formatting.
+s = 'Im here!'
+print('Place my Variable here: %s' %(s))
+
+# Float
+print("Float numer: %1.2f" %(13.145)) # 1.2f Second number is the number of decimals after de point.
+# The first number is the total minimum number of digits th string should contain.
+print("Float numer: %1.5f" %(13.145))
+print("Float numer: %10.2f" %(13.145)) # Is the number isn't that big, it fills with white spaces.
+
+# Convertion Format.
+print("Conver to string: %s" %(123))
+# Or
+print("Conver to string: %s" %(123))
+
+# Prnting multiple variables.
+print("First: %s, Second: %s, Third: %s" %('hi', 'two', 3))
+
+# Proper string formatting.
+print("First: {x} Second: {y} Third: {x}".format(x='inserted',y='Two!'))
