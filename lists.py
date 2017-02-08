0b047a584c61c96e95e7b7127022c9edb71e69a9
Samuel Castillo <samuel.cas.96@gmail.com>
2017-02-07 19:14:27 -0600

    Python Lists

---
+++ lists.py
@@ -0,0 +1,22 @@
+# Lists.
+my_list = [1,2,3]
+print(my_list)
+my_listTwo = ['lel',12,1.2]
+
+# Print the number of elements on the list.
+print(len(my_list))
+
+my_list = ['one','two','three',4,5]
+print(my_list[0])
+print(my_list[1:])
+
+modify_list = my_list + ['new item']
+print(modify_list)
+
+modify_list = my_list*2
+print(modify_list)
+
+# Methods for lists.
+l = [1,2,3]
+l.append('append me!')
+print(l)
